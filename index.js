b7e75c3dc1cf2fad691dcfd6f314ae6c041b0d79
Shams Sujon <sujon2762@gmail.com>
2022-11-10 04:42:10 +0600

    Integrate MongoDB, Get All Services

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// @ts-nocheck
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 7700;
@@ -9,9 +11,33 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Binary Bua server is running");
+	res.send("Binary Bua server is running");
 });
 
 app.listen(port, () => {
-    console.log(`Binary bua server is running on port: ${port}`);
+	console.log(`Binary bua server is running on port: ${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.9q7qmdx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+	try {
+		const serviceCollection = client.db("binaryBuaDB").collection("services");
+
+		// Get all services from DB
+		app.get("/services", async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+	} finally {
+	}
+};
+
+run().catch(console.dir);
